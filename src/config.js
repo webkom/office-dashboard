ff61812d0f5b0830b2d4489703c8e7ac46822d96
kotlarz <marius.kotlarz@gmail.com>
2019-01-16 22:42:34 +0100

    Replace default variable for APIs to their production values

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 export const BRUS_URL =
-  process.env.REACT_APP_BRUS_URL || 'http://localhost:8000';
+  process.env.REACT_APP_BRUS_URL || 'https://brus.abakus.no';
 export const PRESENCE_URL =
-  process.env.REACT_APP_PRESENCE_URL || 'http://localhost:8001';
+  process.env.REACT_APP_PRESENCE_URL || 'https://presence.abakus.no';
 export const ENVIRONMENT_URL =
-  process.env.REACT_APP_ENVIRONMENT_URL || 'http://localhost:8002';
+  process.env.REACT_APP_ENVIRONMENT_URL || 'https://environment-api.abakus.no';
 
 export const OFFICE_SENSORS = ['sensors/2', 'sensors/4', 'sensors/5'];
