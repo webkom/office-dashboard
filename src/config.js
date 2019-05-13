774127ac0d9f884140693a12aedbd80c38778edd
kotlarz <marius.kotlarz@gmail.com>
2019-05-13 22:03:39 +0200

    Use webkom.dev instead of abakus.no for various API requests

--- src/config.js
+++ src/config.js
@@ -1,15 +1,15 @@
 export const BRUS_URL =
   process.env.REACT_APP_BRUS_URL || 'https://brus.abakus.no';
 export const PRESENCE_URL =
-  process.env.REACT_APP_PRESENCE_URL || 'https://presence.abakus.no';
+  process.env.REACT_APP_PRESENCE_URL || 'https://presence.webkom.dev';
 export const ENVIRONMENT_URL =
-  process.env.REACT_APP_ENVIRONMENT_URL || 'https://environment-api.abakus.no';
+  process.env.REACT_APP_ENVIRONMENT_URL || 'https://environment-api.webkom.dev';
 export const KAFFE_URL =
-  process.env.REACT_APP_KAFFE_URL || 'https://kaffe-api.abakus.no';
+  process.env.REACT_APP_KAFFE_URL || 'https://kaffe-api.webkom.dev';
 export const OFFICE_DOOR_URL =
-  process.env.REACT_APP_OFFICE_DOOR_URL || 'https://office-door-api.abakus.no';
+  process.env.REACT_APP_OFFICE_DOOR_URL || 'https://office-door-api.webkom.dev';
 export const OFFICE_CHROMECAST_URL =
   process.env.REACT_APP_OFFICE_CHROMECAST_URL ||
-  'https://office-chromecast-api.abakus.no';
+  'https://office-chromecast-api.webkom.dev';
 
 export const OFFICE_SENSORS = ['sensors/208AAEC40A24', 'sensors/CC6BAFC40A24'];
