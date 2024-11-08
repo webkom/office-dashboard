d52f8f744a74ae7437604d1c8ad75041b8cc62de
PeterJFB <peterj.flbj@gmail.com>
2024-11-08 15:11:01 +0100

    Remove static ip from office times

--- office-dashboard-backend/config.template.py
+++ office-dashboard-backend/config.template.py
@@ -3,4 +3,9 @@ class DevelopmentConfig(object):
     MEMBERS_URI = "members.webkom.dev"
     MEMBERS_USER = ""
     MEMBERS_PASSWORD = ""
+
+    PALANTIR_URI = ""
+    PALANTIR_USER = ""
+    PALANTIR_PASSWORD = ""
+
     GITHUB_API_TOKEN = "create_your_own"
