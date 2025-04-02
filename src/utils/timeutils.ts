77c3940267e28c3bd6efb14cb9483e9909144527
Christoffer Nguyen <christofferhoang@gmail.com>
2025-04-02 19:34:31 +0200

    Live update in seconds, and fixed "x timer siden"

--- src/utils/timeutils.ts
+++ src/utils/timeutils.ts
@@ -1,4 +1,5 @@
-import moment from "moment-timezone";
+import moment, { Moment } from "moment-timezone";
+import "moment/dist/locale/nb";
 
 function formatSecondsToHours(seconds: number) {
   const duration = moment.duration(seconds * 1000);
@@ -11,6 +12,7 @@ function formatSecondsToHours(seconds: number) {
 }
 
 function timeAgo(lastSeen: Date) {
+  moment.locale("nb");
   const lastSeenTime = moment.tz(lastSeen, "Europe/Oslo");
 
   return lastSeenTime.fromNow();
@@ -18,14 +20,24 @@ function timeAgo(lastSeen: Date) {
 
 export const calculateSessionTime = (
   sessionDuration: number, // Current duration calculated by the backend
+  currentTime: Moment,
   lastSeen?: Date,
 ) => {
   if (!lastSeen) return "";
 
+  // Convert lastSeen and currentTime to the same time zone
+  const lastSeenDate = moment.utc(lastSeen).tz("Europe/Oslo");
+
+  // Ensure time difference is calculated correctly
+  const timeDiff = currentTime.diff(lastSeenDate.format(), "seconds");
+
+  // Calculate the difference in seconds
+  const updatedDuration = sessionDuration + timeDiff;
+
   // Convert milliseconds to hours, minutes, and seconds
-  const hours = Math.floor(sessionDuration / 3600); // 1 hour = 3600000 ms
-  const minutes = Math.floor((sessionDuration % 3600) / 60); // 1 minute = 60000 ms
-  const seconds = Math.floor(sessionDuration % 60); // 1 second = 1000 ms
+  const hours = Math.floor(updatedDuration / 3600); // 1 hour = 3600000 ms
+  const minutes = Math.floor((updatedDuration % 3600) / 60); // 1 minute = 60000 ms
+  const seconds = Math.floor(updatedDuration % 60); // 1 second = 1000 ms
 
   return hours > 0
     ? `${hours} t ${minutes} min`
