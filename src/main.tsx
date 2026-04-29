272c88e5bcca8d07d7c1c21be94a1c35ba45792e
Vebjorn Elvekrok <vebjorn.elvekrok@gmail.com>
2026-04-29 15:51:44 +0200

    Fix favicon

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
+    <link rel="icon" type="image/png" href="/favicon.ico" />
     <App />
   </React.StrictMode>,
 );
