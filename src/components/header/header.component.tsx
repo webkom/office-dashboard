8c82405b350da4879d21d963181cf6ac3fd73134
Vebjorn Elvekrok <vebjorn.elvekrok@gmail.com>
2026-04-29 15:41:22 +0200

    Replace abakus logo with webkom

--- src/components/header/header.component.tsx
+++ src/components/header/header.component.tsx
@@ -1,12 +1,10 @@
 import Clock from "./clock/clock.component";
-import darkLogo from "/abakus_logo_white_webkom.png";
+import webkomLogo from "/webkom.png";
 import styles from "./header.module.css";
 import { PropsWithChildren } from "react";
 import Github from "./github/github.component";
 
 const Header = () => {
-  // const isMobile = width !== undefined && width === 'xs';
-  // const isLarge = width !== undefined && ['lg', 'xl'].includes(width);
 
   const HeaderCell: React.FC<PropsWithChildren<{ className?: string }>> = ({
     children,
@@ -21,34 +19,27 @@ const Header = () => {
 
   return (
     <div className={`${styles["header"]} g-flex-row`}>
-      <div className="g-mobile g-flex g-flex-row g-flex-justify-center g-flex-align-center g-p-2">
-        <HeaderCell className="g-flex">
-          <img
-            alt="Abakus Linjeforening"
-            className={styles["logo"]}
-            src={darkLogo}
-          />
-        </HeaderCell>
-      </div>
       <div className="g-not-mobile g-flex g-flex-row g-flex-justify-evenly g-flex-align-center g-p-2">
         <div
           className={`${styles["logo-and-clock"]} g-flex g-flex-row g-flex-justify-start g-flex-align-center`}
         >
           <HeaderCell className="g-flex-col">
-            <img
-              alt="Abakus Linjeforening"
-              className={styles["logo"]}
-              src={darkLogo}
-            />
+            <div className="g-flex-row g-flex-align-center">
+              <img
+                alt="Abakus Linjeforening"
+                className={styles["logo"]}
+                src={webkomLogo}
+              />
+              <h1 className={styles["title"]}>
+                Webkom
+              </h1>
+            </div>
             <Clock />
           </HeaderCell>
         </div>
         <HeaderCell>
           <Github />
         </HeaderCell>
-        {/* <HeaderCell>
-          <Sensors />
-        </HeaderCell> */}
       </div>
     </div>
   );
